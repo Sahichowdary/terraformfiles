dc72a0f1aaa34381141774dec838ff9b4d8b6547
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 11:21:03 +0530

    Update node.tf

--- node.tf
+++ node.tf
@@ -43,8 +43,8 @@ resource "aws_eks_node_group" "private-nodes" {
 
   scaling_config {
     desired_size = 1
-    max_size     = 5
-    min_size     = 0
+    max_size     = 3
+    min_size     = 1
   }
 
   update_config {
