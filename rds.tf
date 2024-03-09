2cf928fc985fba15957800d2ef1d1a735d3c9959
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 18:25:04 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "my-pocsql" {
   publicly_accessible = var.rds.public_access
   storage_type = "standard"
   depends_on = [aws_db_subnet_group.rds_subnetgroup] 
-
+  vpc_security_group_ids = [aws_security_group.db_security_group.id]  # Attach the security group to the RDS instance
   provisioner "local-exec" {
     command = <<-EOT
       mysql -h ${self.endpoint} -u ${var.rds.username} -p${var.rds.password} \
