ab6cb86c47fbdd8b13b76ab7c35f94bfd3df14f4
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 18:31:40 +0530

    Update awswaf.tf

--- awswaf.tf
+++ awswaf.tf
@@ -7,27 +7,36 @@ resource "aws_wafv2_web_acl" "pocawswaf" {
     block {}
   }
 
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "poc-web-acl-metrics"
+    sampled_requests_enabled   = true
+  }
+
   # Rule for blocking requests based on the referrer header
   rule {
     name     = "block-referrer"
     priority = 1
+
     action {
       block {}
     }
+
     statement {
       regex_pattern_set_reference_statement {
+        arn = aws_wafv2_regex_pattern_set.poc.arn
         field_to_match {
           single_header {
             name = "referer"
           }
         }
-        text_transformation {
+        text_transformations {
           priority = 1
           type     = "NONE"
         }
-        regex_pattern_set_id = aws_wafv2_regex_pattern_set.poc.id
       }
     }
+
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "BlockReferrerRule"
@@ -39,6 +48,7 @@ resource "aws_wafv2_web_acl" "pocawswaf" {
 resource "aws_wafv2_regex_pattern_set" "poc" {
   name        = "poc-regex-pattern-set"
   description = "poc Regex Pattern Set"
+  scope       = "CLOUDFRONT"
 
   regular_expression {
     regex_string = "saskenpoc.com"
