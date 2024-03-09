50b37397ef9457d04c404259bb14d987a9cabfca
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 11:01:11 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -30,8 +30,6 @@ resource "aws_eks_cluster" "demo" {
     subnet_ids = [
       aws_subnet.private-us-east-1a.id,
       aws_subnet.private-us-east-1b.id,
-      aws_subnet.public-us-east-1a.id,
-      aws_subnet.public-us-east-1b.id
     ]
   }
 
