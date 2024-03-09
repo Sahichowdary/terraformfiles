3a797345723575f7d129b39a97936b3b902ca61c
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 15:56:20 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,48 @@
+variable "region" {
+    description = "used to specify the region"
+    default = "us-east-1"
+}
+
+variable "availability_zone" {
+    default = "us-east-1a"
+}
+
+variable "availability_zone2" {
+    default = "us-east-1b"
+}
+
+variable "availability_zone3" {
+    default = "us-east-1c"
+}
+
+variable "eks_cluster_role" {
+    default = "arn:aws:iam::036965198866:role/pocEKSClusterRole"
+}
+
+variable "eks_node_role" {
+    default = "arn:aws:iam::036965198866:role/pocAmazonEKSNodeRole"
+}
+
+variable "eks_encryption_key" {
+    default = "arn:aws:kms:us-east-1:036965198866:key/48e8799d-ee0c-4462-a8da-28ae9073ab40"
+}
+
+variable "domain-name" {
+    default = "saskenpoc.com"
+}
+
+variable "loadbalancer_id" {
+    default = " "
+    
+}
+
+variable "rds" {
+  type = object({
+    name = string
+    storage = number
+    engine_version = string
+    username = string
+    password = string
+    public_access = bool
+  })
+}
