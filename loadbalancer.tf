c2f8989eb5fa842291378f9c6dd9c02bb51e0e85
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-11 15:56:37 +0530

    Create loadbalancer.tf

---
+++ loadbalancer.tf
@@ -0,0 +1,72 @@
+
+resource "aws_security_group" "nlb_sg" {
+  name        = "nlb-sg"
+  description = "Security group for NLB"
+  vpc_id      = aws_vpc.private_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nlb-sg"
+  }
+}
+
+
+# Create Network Load Balancer
+resource "aws_lb" "nlb" {
+  name               = "poc-nlb"
+  load_balancer_type = "network"
+  subnets            = [private-us-east-1a, private-us-east-1b, public-us-east-1a, public-us-east-1b]
+  security_groups    = [aws_security_group.nlb_sg.id]
+}
+
+# Create VPC security group for EKS nodes
+resource "aws_security_group" "eks_sg" {
+  name        = "eks-sg"
+  description = "Security group for EKS nodes"
+  vpc_id      = aws_vpc.private_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-sg"
+  }
+}
+
+# Create Target Group for EKS Cluster
+resource "aws_lb_target_group" "eks_target_group" {
+  name        = "eks-target-group"
+  port        = 443
+  protocol    = "HTTPS"
+  vpc_id      = aws_vpc.private_vpc.id
+  target_type = "ip"
+  health_check {
+    protocol = "HTTPS"
+    path     = "/"
+    port     = "traffic-port"
+  }
+}
+
+# Attach EKS Cluster Nodes to Target Group
+resource "aws_autoscaling_attachment" "eks_attachment" {
+  depends_on             = [aws_lb_target_group.eks_target_group]
+  autoscaling_group_name = "aws_eks_node_group.private-nodes"
+  alb_target_group_arn  = aws_lb_target_group.eks_target_group.arn
+}
