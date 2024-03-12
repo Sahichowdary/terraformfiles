5e5e4c690aafef30ea71154ea7d9e4170ce937ca
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-13 01:00:28 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -11,11 +11,4 @@ provider "aws" {
   region = var.region
 }
 
-backend "s3" {
-  bucket         = "pocterraformbackendjava"
-  key            = "terraform.tfstate"
-  region         = var.region
-  dynamodb_table = "terraform_locks"
-  encrypt        = true
-}
 
