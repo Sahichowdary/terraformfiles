e91946d1e9d7c6beef27a369f8ade1180c4f455f
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-12 16:52:11 +0530

    Create bastion.tf

---
+++ bastion.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-security-group"
+  description = "Security group for the bastion host"
+  vpc_id      = aws_vpc.private.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing SSH access from anywhere, you may restrict it as per your requirements
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# Create bastion host
+resource "aws_instance" "bastion_host-POC" {
+  instance_type   = "t2.micro" # Set your desired instance type
+  subnet_id       = aws_subnet.public-us-east-1a.id
+  security_groups = [aws_security_group.bastion_sg.name]
+  key_name        = "aws-poc-demo"                          # Set your key name for SSH access
+}
+
+
+resource "aws_security_group_rule" "db_from_bastion" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  security_group_id = aws_db_instance.my-pocsql.vpc_security_group_ids
+  source_security_group_id = aws_security_group.bastion_sg.id
+}
+
