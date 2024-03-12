2ff19835d5d4ac2123d25a08358ad39fcc604790
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-13 00:48:32 +0530

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -33,15 +33,13 @@ resource "aws_instance" "bastion_host-POC" {
   security_groups = bastion-security-group
    key_name        = "aws-poc-demo"                          # Set your key name for SSH access 
    user_date   =
-               <<-EOF
               #!/bin/bash
               apt sudo apt install mysql-server
               mysql -h ${aws_db_instance.endpoint.my-pocsql.dns_name} -u ${var.rds.username} -p${var.rds.password} 
-              -e "CREATE DATABASE IF NOT EXISTS foodfinder;
+              CREATE DATABASE IF NOT EXISTS foodfinder;
               USE foodfinder;
               CREATE TABLE IF NOT EXISTS users (first_name VARCHAR(255), last_name VARCHAR(255), username VARCHAR(255), email VARCHAR(255), password VARCHAR(255)); 
-              INSERT INTO users (first_name, last_name, username, email, password) VALUES ('raj', 'kapoor', 'rajKapoor', 'raj.kapoor@gmail.com', 'rajKapoor');"
-              EOF->>
+              INSERT INTO users (first_name, last_name, username, email, password) VALUES ('raj', 'kapoor', 'rajKapoor', 'raj.kapoor@gmail.com', 'rajKapoor');
  
 }
 
