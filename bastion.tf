97aca98a90206265068c74272fc41d0281c15099
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-13 00:38:43 +0530

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -9,6 +9,12 @@ resource "aws_security_group" "bastion_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allowing SSH access from anywhere, you may restrict it as per your requirements
   }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing SSH access from anywhere, you may restrict it as per your requirements
+  }
 
   egress {
     from_port   = 0
@@ -25,7 +31,18 @@ resource "aws_instance" "bastion_host-POC" {
   ami             = "ami-07d9b9ddc6cd8dd30"
   subnet_id       = aws_subnet.public-us-east-1a.id
   security_groups = bastion-security-group
-  key_name        = "aws-poc-demo"                          # Set your key name for SSH access
+   key_name        = "aws-poc-demo"                          # Set your key name for SSH access 
+   user_date   =
+               <<-EOF
+              #!/bin/bash
+              apt sudo apt install mysql-server
+              mysql -h ${aws_db_instance.endpoint.my-pocsql.dns_name} -u ${var.rds.username} -p${var.rds.password} 
+              -e "CREATE DATABASE IF NOT EXISTS foodfinder;
+              USE foodfinder;
+              CREATE TABLE IF NOT EXISTS users (first_name VARCHAR(255), last_name VARCHAR(255), username VARCHAR(255), email VARCHAR(255), password VARCHAR(255)); 
+              INSERT INTO users (first_name, last_name, username, email, password) VALUES ('raj', 'kapoor', 'rajKapoor', 'raj.kapoor@gmail.com', 'rajKapoor');"
+              EOF->>
+ 
 }
 
 
