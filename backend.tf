81c9cff00f6ae43b6e8234bec92479dd7b37b512
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-13 06:13:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
       bucket = "pocterraformbackendjava"
       key = "app-state"
       region = "us-east-1"
-      dynamodb_table = "terraform_locks"
       encrypt        = true
 
     }
