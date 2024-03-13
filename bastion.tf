1f44e18b96545bed443a644721f38190ff174bcd
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-13 11:34:56 +0530

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -9,11 +9,12 @@ resource "aws_security_group" "bastion_sg-poc" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allowing SSH access from anywhere, you may restrict it as per your requirements
   }
+
   ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing SSH access from anywhere, you may restrict it as per your requirements
+    cidr_blocks = ["0.0.0.0/0"] # Allowing MySQL access from anywhere, you may restrict it as per your requirements
   }
 
   egress {
@@ -22,35 +23,35 @@ resource "aws_security_group" "bastion_sg-poc" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "bastion-security-group-poc"
-     }
+  }
 }
 
-
 resource "aws_instance" "bastion_host-POC" {
-  instance_type   = "t2.micro"                                 # Set your desired instance type
+  instance_type   = "t2.micro"
   ami             = "ami-07d9b9ddc6cd8dd30"
   subnet_id       = aws_subnet.public-us-east-1a.id
-  security_groups = [aws_security_group.bastion_sg-poc.id]        # Corrected reference to security group name
-  key_name        = "aws-poc-demo"                              # Set your key name for SSH access 
-  depends_on = [aws_db_instance.my-pocsql, aws_security_group.bastion_sg-poc]
-  lifecycle { 
-          ignore_changes = [security_groups] }
+  security_groups = [aws_security_group.bastion_sg-poc.id]
+  key_name        = "aws-poc-demo"
+  depends_on      = [aws_db_instance.my-pocsql, aws_security_group.bastion_sg-poc]
+
+  lifecycle {
+    ignore_changes = [security_groups]
+  }
+
   tags = {
     Name = "Bastion by Terraform"
   }
 }
 
-
-
-
 resource "aws_security_group_rule" "db_from_bastion" {
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  security_group_id = aws_security_group.db_security_group.id
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.db_security_group.id
   source_security_group_id = aws_security_group.db_security_group.id
 }
 
