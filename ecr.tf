49218c24d3056285d1b066856aa8bcf9e627391a
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-13 11:35:50 +0530

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "poc_ecr_java" {
-  name                 = "poc_ecr_java"
+  name                 = "FoodFinder"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
