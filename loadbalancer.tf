7313ac3fa67f00e5a3493a4ac66ef834e1a791d2
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-13 08:09:32 +0530

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -39,9 +39,9 @@ resource "aws_security_group" "eks_sg" {
   vpc_id      = aws_vpc.private_vpc.id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "https"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
