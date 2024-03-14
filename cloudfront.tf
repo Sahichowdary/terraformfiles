5de4e0c7cbcb46f79357952dcf4b5bf5f3e9dd41
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-14 18:18:09 +0530

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,101 +1,42 @@
-
-# Create a security group for CloudFront
-resource "aws_security_group" "cloudfront_sg" {
-  name        = "cloudfront-sg"
-  description = "Security group for CloudFront"
-  vpc_id      = aws_vpc.private_vpc.id  # Specify the VPC ID where CloudFront will be deployed
-
-  ingress {
-    from_port   = 80
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allowing traffic from anywhere, you may restrict it as per your requirements
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "cloudfront-sg"
-  }
-}
-
-
-# Update CloudFront distribution to point to NLB
-resource "aws_cloudfront_distribution" "eks_cloudfront_distribution" {
-  depends_on = [aws_lb.nlb] 
+resource "aws_cloudfront_distribution" "ekscdn" {
   origin {
-    domain_name = data.aws_elb.elbfood.dns_name
-    origin_id   = "data.aws_elb.elbfood"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "https-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
+    domain_name = aws_elb.eldfood.dns_name
+    origin_id   = "ELBOrigin"
   }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "data.aws_elb.elbfood"
+    target_origin_id = "ELBOrigin"
 
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
     forwarded_values {
       query_string = false
-
       cookies {
         forward = "none"
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
+    compress               = true
+    smooth_streaming       = false
+    viewer_certificate {
+      cloudfront_default_certificate = true
     }
   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "EKS CloudFront Distribution"
-  default_root_object = "index.html"
-
-  # Add SSL certificate from ACM
-  viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.cert.arn
-    ssl_support_method  = "sni-only"
-  }
-
-  tags = {
-    Name = "eks-cloudfront-distribution"
-  }
-}
-
-
-resource "aws_security_group_rule" "cloudfront_to_nlb" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.nlb_sg.id
-  source_security_group_id = aws_security_group.cloudfront_sg.id
-}
-
 output "cloudfront_distribution_arn" {
-  value = aws_cloudfront_distribution.eks_cloudfront_distribution.arn
+  value = aws_cloudfront_distribution.ekscdn.arn
 }
 
 
 output "cloudfront_url" {
-  value = aws_cloudfront_distribution.eks_cloudfront_distribution.domain_name
+  value = aws_cloudfront_distribution.ekscdn.domain_name
 }
