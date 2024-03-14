dbd20da147502021287d7ca405cdd307e1d04346
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-14 19:27:37 +0530

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -4,13 +4,13 @@ data "aws_elb" "elbfood" {
 }
 
 resource "aws_lb_listener" "nlb_listener_https" {
-  load_balancer_arn = data.aws_lb.elbfood.arn
+  load_balancer_dns_name = ae2ffedd0f3594043b798c79362e7157-1561109267.us-east-1.elb.amazonaws.com
   port              = 443
   protocol          = "TLS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.nlb_target_group.arn
+    
   }
 }
