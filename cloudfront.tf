3a993d5156a29846a5813da7b85af7606e83d9b8
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-14 18:25:56 +0530

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -33,6 +33,7 @@ resource "aws_cloudfront_distribution" "ekscdn" {
         acm_certificate_arn = aws_acm_certificate.cert.arn
         ssl_support_method  = "sni-only"
   }
+ }
 }
 
 output "cloudfront_distribution_arn" {
