3c14483d48c94ea636d872953913762424fa7930
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-14 14:19:27 +0530

    Update nodegroup.tf

--- nodegroup.tf
+++ nodegroup.tf
@@ -28,6 +28,11 @@ resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadO
   role       = aws_iam_role.nodes.name
 }
 
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryPowerUser" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+  role       = aws_iam_role.nodes.name
+}
+
 resource "aws_eks_node_group" "private-nodes" {
   cluster_name    = aws_eks_cluster.demo.name
   node_group_name = "private-nodes"
