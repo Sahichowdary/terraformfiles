b9dc84f89a7913a4abc0826c1ae8f435e199ebe9
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-14 19:24:02 +0530

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -3,4 +3,14 @@ data "aws_elb" "elbfood" {
   name = var.lb_name
 }
 
-
+resource "aws_lb_listener" "nlb_listener_https" {
+  load_balancer_arn = data.aws_lb.elbfood.arn
+  port              = 443
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nlb_target_group.arn
+  }
+}
