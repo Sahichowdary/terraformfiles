f4c8ee9e09faea027846ba0e8bea911f7b045499
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-14 18:41:42 +0530

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,31 +1,35 @@
-resource "aws_cloudfront_distribution" "ekscdn" {
+
+# Update CloudFront distribution to point to NLB
+resource "aws_cloudfront_distribution" "eks_cloudfront_distribution" {
   origin {
     domain_name = data.aws_elb.elbfood.dns_name
-    origin_id   = data.aws_elb.elbfood.id
+    origin_id   = "data.aws_elb.elbfood"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
   default_cache_behavior {
-    target_origin_id       = data.aws_elb.elbfood.id
-    viewer_protocol_policy = "redirect-to-https"
-
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "data.aws_elb.elbfood"
+
     forwarded_values {
       query_string = false
+
       cookies {
         forward = "none"
       }
     }
 
-    min_ttl          = 0
-    default_ttl      = 3600
-    max_ttl          = 86400
-    compress         = true
-    smooth_streaming = false
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
   restrictions {
@@ -34,17 +38,28 @@ resource "aws_cloudfront_distribution" "ekscdn" {
     }
   }
 
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "EKS CloudFront Distribution"
+  default_root_object = "index.html"
+
+  # Add SSL certificate from ACM
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.cert.arn
     ssl_support_method  = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2018"
+  }
+
+  tags = {
+    Name = "eks-cloudfront-distribution"
   }
 }
 
+
 output "cloudfront_distribution_arn" {
-  value = aws_cloudfront_distribution.ekscdn.arn
+  value = aws_cloudfront_distribution.eks_cloudfront_distribution.arn
 }
 
+
 output "cloudfront_url" {
-  value = aws_cloudfront_distribution.ekscdn.domain_name
+  value = aws_cloudfront_distribution.eks_cloudfront_distribution.domain_name
 }
