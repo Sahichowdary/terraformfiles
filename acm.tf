5c0f657b17373609a60b58ffd291f17541a75bfe
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-15 12:44:10 +0530

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -23,8 +23,7 @@ resource "aws_acm_certificate_validation" "cert" {
 resource "aws_route53_record" "acm_validation" {
   count   = length(aws_acm_certificate.example.domain_validation_options)
   zone_id = "data.aws_route53_zone.zone.zone_id"
-name = saskenpoc.com 
-  aws_acm_certificate.example.domain_validation_options[count.index].resource_record_name
+  name = aws_acm_certificate.example.domain_validation_options[count.index].resource_record_name
   type    = aws_acm_certificate.example.domain_validation_options[count.index].resource_record_type
   records = [aws_acm_certificate.example.domain_validation_options[count.index].resource_record_value]
   ttl     = 60
